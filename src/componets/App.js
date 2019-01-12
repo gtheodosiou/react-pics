ab967e28e89d625f71f15a5a93539a886734ec94
George <theodgeorge@gmail.com>
2019-01-12 16:02:20 +0000

    Make App a class component and pass over the props

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Searchbar from './SearchBar';
+import SearchBar from './SearchBar';
 
 
-const App = () => {
-    return <div className="ui container" style={{marginTop: '10px'}}>
-        <Searchbar />
-        </div>;
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+    render () {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        )
+    }
 };
 
 export default App;
